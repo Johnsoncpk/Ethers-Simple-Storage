42c28a352d37d3449a84366adeb5d9919918d389
Johnsoncpk <johnsonc@Johnsonc.localdomain>
2022-06-12 18:19:05 +0800

    ganache

--- deploy.js
+++ deploy.js
@@ -1,13 +1,30 @@
 const ethers = require("ethers");
+const fs = require("fs-extra");
 require('dotenv').config()
 
 async function main() {
     // http://127.0.0.1:7545
     const provider = new ethers.providers.JsonRpcProvider(process.env.LocalProvider);
+    console.log('Waiting for network')
+    const network = await provider.detectNetwork()
+    console.log(network)
     const wallet = new ethers.Wallet(
         process.env.PrivateKey,
         provider
     );
+    const abi = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.abi",
+        "utf8"
+    );
+    const binary = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.bin",
+        "utf8"
+    );
+
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+    console.log("Deploying, please wait...");
+    const contract = await contractFactory.deploy();
+    console.log(contract);
 }
 
 main()

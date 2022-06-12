e4b2120ef74b6997fba3887eb74e051d8d3c09a1
Johnsoncpk <chong12319@gmail.com>
2022-06-12 23:32:24 +0800

    middle of something

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
-require('dotenv').config()
+require('dotenv').config();
 
 async function main() {
     // http://127.0.0.1:7545
@@ -8,10 +8,19 @@ async function main() {
     console.log('Waiting for network')
     const network = await provider.detectNetwork()
     console.log(network)
-    const wallet = new ethers.Wallet(
-        process.env.PrivateKey,
-        provider
+
+    // const wallet = new ethers.Wallet(
+    //     process.env.PrivateKey,
+    //     provider
+    // );
+
+    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+
+    let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+        encryptedJson,
+        process.env.PRIVATE_KEY_PASSWORD
     );
+
     const abi = fs.readFileSync(
         "./SimpleStorage_sol_SimpleStorage.abi",
         "utf8"
@@ -22,9 +31,16 @@ async function main() {
     );
 
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+
     console.log("Deploying, please wait...");
     const contract = await contractFactory.deploy();
-    console.log(contract);
+    await contract.deployTransaction.wait(1);
+
+
+    console.log(currentFavouriteNumber.toString());
+    const transactionResponse = await contract.store("652");
+    const currentFavouriteNumber = await contract.retrieve();
+    console.log(currentFavouriteNumber.toString());
 }
 
 main()
